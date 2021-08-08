1913a57731af8afed80f012645bcb6b4310faf25
Kyle <devbetter@yahoo.com>
2021-08-08 23:53:21 +0700

    update for Guid Generator

--- webapp/apps/Dev.Web.UFD/Areas/Controllers/GuidGeneratorController.cs
+++ webapp/apps/Dev.Web.UFD/Areas/Controllers/GuidGeneratorController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dev.Web.UFD.Areas.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dev.Web.UFD.Areas.Controllers
 {
@@ -11,7 +8,21 @@ namespace Dev.Web.UFD.Areas.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            GuidGeneratorModel guidGeneratorModel = new GuidGeneratorModel();
+            return View(guidGeneratorModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(GuidGeneratorModel model)
+        {
+            string guidsInString = string.Empty;
+            for (int i = 0; i < model.NumberOfGuids; i++)
+            {
+                guidsInString += System.Guid.NewGuid().ToString() + System.Environment.NewLine;
+            }
+
+            model.GuidGenerateInString = guidsInString;
+            return View(model);
         }
     }
 }
---
+++ webapp/apps/Dev.Web.UFD/Areas/Models/GuidGeneratorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Dev.Web.UFD.Areas.Models
+{
+    public class GuidGeneratorModel
+    {
+        public GuidGeneratorModel()
+        {
+            NumberOfGuids = 1;
+            GuidGenerateInString = string.Empty;
+        }
+        public int NumberOfGuids { get; set; }
+
+        public string GuidGenerateInString { get; set; }
+    }
+}
